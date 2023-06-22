074612ac3e04d5487585231999cbaffc5ed51168
TazWanted <vascowbrito@gmail.com>
2023-06-22 13:41:58 +0100

    Deteta as mensagens no telemovel

--- app/src/main/java/com/example/internal_calendar_phone/MainActivity.java
+++ app/src/main/java/com/example/internal_calendar_phone/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.internal_calendar_phone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.RECEIVE_SMS)
+        != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.RECEIVE_SMS}, 100);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/internal_calendar_phone/postActivity.java
@@ -0,0 +1,51 @@
+package com.example.internal_calendar_phone;
+
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class postActivity extends AppCompatActivity {
+
+    private void sendPostRequest() {
+        String url = "http://localhost:3000/api/mobile";
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Sucesso na resposta do servidor
+                        Toast.makeText(postActivity.this, "POST request successful!", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Erro na resposta do servidor
+                        Toast.makeText(postActivity.this, "POST request failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("param1", "value1");
+                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(postRequest);
+    }
+
+}
