908915c8aad45554fb4fe844c232bd5cfd6e00fe
TazWanted <vascowbrito@gmail.com>
2023-06-23 10:29:01 +0100

    Deteta e envia para a restAPI, proximo objetivo e fazer a app correr em 2º plano

--- app/src/main/java/com/example/internal_calendar_phone/postActivity.java
+++ app/src/main/java/com/example/internal_calendar_phone/postActivity.java
@@ -2,6 +2,7 @@ package com.example.internal_calendar_phone;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,9 +12,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,36 +46,44 @@ public class postActivity extends AppCompatActivity {
     }
 
     public void sendPostRequest(Context context, String message) {
-        Toast.makeText(context, "Mensagem aqui?: " + message, Toast.LENGTH_SHORT).show();
-        String url = "http://localhost:3000/api/phone/msg";
+        String url = "http://192.168.1.86:3000/api/phone/msg";
 
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("param1", "value1");
+            jsonParams.put("param2", "value2");
+            jsonParams.put("msgBody", message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, jsonParams,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(JSONObject response) {
                         // Sucesso na resposta do servidor
-                        Toast.makeText(postActivity.this, "POST request successful!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context, "POST request successful!", Toast.LENGTH_SHORT).show();
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // Erro na resposta do servidor
-                        Toast.makeText(postActivity.this, "POST request failed!", Toast.LENGTH_SHORT).show();
+                        String responseBody = "";
+                        if (error.networkResponse != null && error.networkResponse.data != null) {
+                            try {
+                                responseBody = new String(error.networkResponse.data, "utf-8");
+                            } catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.e("PostActivity", "Error response: " + responseBody);
+                        Toast.makeText(context.getApplicationContext(), "POST request failed!", Toast.LENGTH_SHORT).show();
                     }
                 }
-        ) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("param1", "value1");
-                params.put("param2", "value2");
-                params.put("msgBody", message); // Adiciona o msgBody aos parâmetros do POST
-                return params;
-            }
-        };
+        );
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(context.getApplicationContext());
         requestQueue.add(postRequest);
     }
 }
